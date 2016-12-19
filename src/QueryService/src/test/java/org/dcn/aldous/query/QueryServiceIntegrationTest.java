3faabe00b9a1820d6470d7e5f06ce9bddd165c26
OP <oleparh@gmail.com>
2016-12-19 18:21:25 +0300

    integration test simulates load - 1000 users

--- src/QueryService/src/test/java/org/dcn/aldous/query/QueryServiceIntegrationTest.java
+++ src/QueryService/src/test/java/org/dcn/aldous/query/QueryServiceIntegrationTest.java
@@ -7,6 +7,9 @@ import com.google.common.net.HostAndPort;
 import io.dropwizard.Configuration;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.dcn.aldous.database.items.Item;
 import org.dcn.aldous.database.lists.ItemList;
 import org.dcn.aldous.query.services.rest.AuthenticationRest;
@@ -42,53 +45,20 @@ public class QueryServiceIntegrationTest {
       new DropwizardAppRule<>(QueryServiceApplication.class, ResourceHelpers.resourceFilePath("rest.yml"));
 
   @Test
-  public void testAppMainScenario() throws IOException {
-    String username = UUID.randomUUID().toString();
-    String password = "pass";
-    Form newUser = new Form()
-        .param("name", "John Doe")
-        .param("username", username)
-        .param("password", password);
-    Response add = newRequest(UsersRest.class, "add")
-        .post(Entity.form(newUser));
-    assertThat(add.getStatus()).isEqualTo(200);
-    Integer userId = add.readEntity(Integer.class);
+  public void testAppMainScenario() {
+    for (int i = 0; i < 1000; i++) {
+      new Thread(this::userScenario).start();
+    }
+  }
 
-    Form authForm = new Form()
-        .param("login", username)
-        .param("password", password);
-    Response auth = newRequest(AuthenticationRest.class, "byPassword")
-        .post(Entity.form(authForm));
-    assertThat(auth.getStatus()).isEqualTo(200);
-    NewCookie session = auth.getCookies().getOrDefault(AuthenticationRest.ALDOUS_SESSION, null);
-    assertThat(session).isNotNull();
-
-    String sessionId = session.getValue();
-    String listName = String.format("List %.0f", Math.random() * 100);
-    Integer listId = newAuthorizedRequest(ListsRest.class, "addList",
-        params("name", listName), sessionId)
-        .get(Integer.class);
-
-    String jsonBody = newAuthorizedRequest(MatchingItemsRest.class, "getMatchingItems",
-        params("query", "Samsung"), sessionId)
-        .get(String.class);
-    ObjectMapper mapper = new ObjectMapper();
-    TypeFactory typeFactory = mapper.getTypeFactory();
-    ArrayType type = typeFactory.constructArrayType(Item.class);
-    List<Item> items = Arrays.asList(mapper.readValue(jsonBody, type));
-
-    Integer itemId1 = addRandomItem(sessionId, listId, items);
-    Integer itemId2 = addRandomItem(sessionId, listId, items);
-    Integer itemId3 = addRandomItem(sessionId, listId, items);
-
-    ItemList list = newAuthorizedRequest(ListsRest.class, "getList",
-        params("listId", listId), sessionId)
-        .get(ItemList.class);
-
-    assertThat(list.getId()).isEqualTo(listId);
-    assertThat(list.getName()).isEqualTo(listName);
-    assertThat(list.getOwnerId()).isEqualTo(userId);
-    assertThat(list.getItemIds()).contains(itemId1, itemId2, itemId3);
+  @SneakyThrows(IOException.class)
+  protected void userScenario() {
+    TestUser user = new TestUser(UUID.randomUUID().toString(), "pass");
+    user.register();
+    user.signIn();
+    for (int i = 0; i < 4; i++) {
+      user.addListWithContents();
+    }
   }
 
   protected Integer addRandomItem(String sessionId, Integer listId, List<Item> items) {
@@ -143,4 +113,66 @@ public class QueryServiceIntegrationTest {
     return restClass.getAnnotation(Path.class).value();
   }
 
+  @RequiredArgsConstructor
+  private class TestUser {
+
+    private final String username;
+
+    private final String password;
+
+    private Integer userId;
+
+    private String sessionId;
+
+    public void register() {
+      Form newUser = new Form()
+          .param("name", "John Doe")
+          .param("username", username)
+          .param("password", password);
+      Response add = newRequest(UsersRest.class, "add")
+          .post(Entity.form(newUser));
+      assertThat(add.getStatus()).isEqualTo(200);
+      userId = add.readEntity(Integer.class);
+    }
+
+    public void signIn() {
+      Form authForm = new Form()
+          .param("login", username)
+          .param("password", password);
+      Response auth = newRequest(AuthenticationRest.class, "byPassword")
+          .post(Entity.form(authForm));
+      assertThat(auth.getStatus()).isEqualTo(200);
+      NewCookie session = auth.getCookies().getOrDefault(AuthenticationRest.ALDOUS_SESSION, null);
+      assertThat(session).isNotNull();
+      sessionId = session.getValue();
+    }
+
+    public void addListWithContents() throws IOException {
+      String listName = String.format("List %.0f", Math.random() * 100);
+      Integer listId = newAuthorizedRequest(ListsRest.class, "addList",
+          params("name", listName), sessionId)
+          .get(Integer.class);
+
+      String jsonBody = newAuthorizedRequest(MatchingItemsRest.class, "getMatchingItems",
+          params("query", "Samsung"), sessionId)
+          .get(String.class);
+      ObjectMapper mapper = new ObjectMapper();
+      TypeFactory typeFactory = mapper.getTypeFactory();
+      ArrayType type = typeFactory.constructArrayType(Item.class);
+      List<Item> items = Arrays.asList(mapper.readValue(jsonBody, type));
+
+      Integer itemId1 = addRandomItem(sessionId, listId, items);
+      Integer itemId2 = addRandomItem(sessionId, listId, items);
+      Integer itemId3 = addRandomItem(sessionId, listId, items);
+
+      ItemList list = newAuthorizedRequest(ListsRest.class, "getList",
+          params("listId", listId), sessionId)
+          .get(ItemList.class);
+
+      assertThat(list.getId()).isEqualTo(listId);
+      assertThat(list.getName()).isEqualTo(listName);
+      assertThat(list.getOwnerId()).isEqualTo(userId);
+      assertThat(list.getItemIds()).contains(itemId1, itemId2, itemId3);
+    }
+  }
 }
